6b8cfa42067995cfad5844c2dbf0d9c2b96e4722
Teklay <teklay@gmail.com>
2022-11-13 14:59:43 +0300

    Add javascript file

---
+++ index.js
@@ -0,0 +1,17 @@
+let menu = document.getElementById("menu-mobile");
+let menu_btn = document.getElementById("menu-bar");
+let close_bar = document.getElementsByClassName("close")[0];
+
+menu_btn.onclick = function() {
+    menu.style.display = "block";
+}
+
+close_bar.onclick = function() {
+    menu.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target !== menu) {
+      modal.style.display = "none";
+    }
+}
