4c5a8701e7a3581493c99deb02627063fa72dc2a
Teklay <teklay@gmail.com>
2022-11-16 04:28:23 +0300

    Add daynamic contents to the work section and popup menu

--- index.js
+++ index.js
@@ -1,6 +1,105 @@
 const menu = document.getElementById('menu-mobile');
 const menubtn = document.getElementById('menu-bar');
 const closebar = document.getElementsByClassName('close')[0];
+const workid = document.querySelector('#work-id');
+// const body = document.querySelector('body');
+const worksection = document.querySelector('.work-container-card');
+worksection.innerHTML = `<div class="work-container">
+<div class="img-placer">
+    <img src="images/im1.png" alt="Multi-Post Stories Gain+Glory">
+</div>
+<div class="txt-info">
+    <h2 class="work-title">Multi-Post Stories Gain+Glory</h2>
+    <ul class="languages">
+        <li><a href="">Ruby on rails</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">JavaScript</a></li>
+        <li><a href="">html</a></li>
+    </ul>
+    <button class="see-project" type="button">
+        <span class="project-title">See Project</span> </button>
+</div>
+</div>
+<div class="work-container">
+<div class="img-placer">
+    <img src="images/im1.png" alt="My second project sample image">
+</div>
+<div class="txt-info">
+    <h2 class="work-title">Multi-Post Stories Gain+Glory</h2>
+    <ul class="languages">
+        <li><a href="#">Ruby on rails</a></li>
+        <li><a href="#">css</a></li>
+        <li><a href="#">JavaScript</a></li>
+        <li><a href="#">html</a></li>
+    </ul>
+    <button class="see-project" type="button">
+        <span class="project-title">See Project</span> </button>
+</div>
+</div>
+<div class="work-container">
+<div class="img-placer">
+    <img src="images/im1.png" alt="My third project sample image">
+</div>
+<div class="txt-info">
+    <h2 class="work-title">Multi-Post Stories Gain+Glory</h2>
+    <ul class="languages">
+        <li><a href="">Ruby on rails</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">JavaScript</a></li>
+        <li><a href="">html</a></li>
+    </ul>
+    <button class="see-project" type="button">
+        <span class="project-title">See Project</span> </button>
+</div>
+</div>
+<div class="work-container">
+<div class="img-placer">
+    <img src="images/im1.png" alt="My fourth project sample image">
+</div>
+<div class="txt-info">
+    <h2 class="work-title">Multi-Post Stories Gain+Glory</h2>
+    <ul class="languages">
+        <li><a href="">Ruby on rails</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">JavaScript</a></li>
+        <li><a href="">html</a></li>
+    </ul>
+    <button class="see-project" type="button">
+        <span class="project-title">See Project</span> </button>
+</div>
+</div>
+<div class="work-container">
+<div class="img-placer">
+    <img src="images/im1.png" alt="My fifth project sample image">
+</div>
+<div class="txt-info">
+    <h2 class="work-title">Multi-Post Stories Gain+Glory</h2>
+    <ul class="languages">
+        <li><a href="">Ruby on rails</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">JavaScript</a></li>
+        <li><a href="">html</a></li>
+    </ul>
+    <button class="see-project" type="button">
+        <span class="project-title">See Project</span> </button>
+</div>
+</div>
+<div class="work-container">
+<div class="img-placer">
+    <img src="images/im1.png" alt="My sixth project sample image">
+</div>
+<div class="txt-info">
+    <h2 class="work-title">Multi-Post Stories Gain+Glory</h2>
+    <ul class="languages">
+        <li><a href="">Ruby on rails</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">JavaScript</a></li>
+        <li><a href="">html</a></li>
+    </ul>
+    <button class="see-project" type="button">
+        <span class="project-title">See Project</span> </button>
+</div>
+</div>`;
 
 menubtn.onclick = function display() {
   menu.style.display = 'block';
@@ -19,3 +118,122 @@ window.onclick = function exit(event) {
 menu.onclick = function close() {
   menu.style.display = 'none';
 };
+
+const details = document.querySelectorAll('.see-project');
+
+const WorkDatas = [
+  {
+    id: 0,
+    url: './images/Snapshoot_Portfolio-mobile.png',
+    title: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    tecnologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linklive: 'https://tekhlay.github.io/microverse-portfolio-website/',
+    linksource: 'https://github.com/Tekhlay/microverse-portfolio-website',
+  },
+  {
+    id: 1,
+    url: './images/Snapshoot_Portfolio-mobile.png',
+    title: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    tecnologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linklive: 'https://tekhlay.github.io/microverse-portfolio-website/',
+    linksource: 'https://github.com/Tekhlay/microverse-portfolio-website',
+  },
+  {
+    id: 2,
+    url: './images/Snapshoot_Portfolio-mobile.png',
+    title: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    tecnologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linklive: 'https://tekhlay.github.io/microverse-portfolio-website/',
+    linksource: 'https://github.com/Tekhlay/microverse-portfolio-website',
+  },
+  {
+    id: 3,
+    url: './images/Snapshoot_Portfolio-mobile.png',
+    title: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    tecnologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linklive: 'https://tekhlay.github.io/microverse-portfolio-website/',
+    linksource: 'https://github.com/Tekhlay/microverse-portfolio-website',
+  },
+  {
+    id: 4,
+    url: './images/Snapshoot_Portfolio-mobile.png',
+    title: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    tecnologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linklive: 'https://tekhlay.github.io/microverse-portfolio-website/',
+    linksource: 'https://github.com/Tekhlay/microverse-portfolio-website',
+  },
+  {
+    id: 5,
+    url: './images/Snapshoot_Portfolio-mobile.png',
+    title: 'Keeping track of hundreds of components',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    tecnologies: {
+      tech1: 'Ruby on rails',
+      tech2: 'css',
+      tech3: 'JavaScript',
+      tech4: 'html',
+    },
+    linklive: 'https://tekhlay.github.io/microverse-portfolio-website/',
+    linksource: 'https://github.com/Tekhlay/microverse-portfolio-website',
+  },
+];
+
+const workdetailsSection = (content) => `
+    <span class="close-details">&times;</span>
+    <img class="imag-details" src=${WorkDatas[content].url} alt="Image details">
+    <h2 class="project-name">${WorkDatas[content].title}</h2>
+    <ul class="detail-tags">
+        <li><a href="">${WorkDatas[content].tecnologies.tech1}</a></li>
+        <li><a href="">${WorkDatas[content].tecnologies.tech2}</a></li>
+        <li><a href="">${WorkDatas[content].tecnologies.tech3}</a></li>
+    </ul>
+    <p class="project-desc">${WorkDatas[content].description}</p>
+    <div class="btn-detail">
+        <a href="${WorkDatas[content].linklive}">See Live <i class="fa fa-external-link"></i></a>
+        <a href="${WorkDatas[content].linksource}"> See Source <i class="fa fa-github"></i></a>
+    </div>
+ `;
+
+  for (let i = 0; i < details.length; i++) {
+    details[i].addEventListener('click', () => {
+      const create_div = document.createElement('div');
+      create_div.className = "work-details-card";
+      create_div.innerHTML = workdetailsSection(i);
+      workid.appendChild(create_div);
+      // document.body.appendChild(create_div);
+      const closedetails = document.querySelector('.close-details');
+      closedetails.addEventListener('click', async =>{
+        workid.removeChild(create_div);
+      }) 
+    });
+  }
